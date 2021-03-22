67ea3b4e4708a7e80b8e322cec9ebb6a913c72c8
juliobriones <julesc00@hotmail.com>
2021-03-21 20:22:41 -0600

    Added some authentication and permissions.

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,12 +1,23 @@
+from django.contrib.auth.models import User
+
 from rest_framework import serializers
 
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ["id", "username", "snippets"]
+
+
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
-        fields = ["id", "title", "code", "linenos", "language", "style"]
+        fields = ["id", "owner", "title", "code", "linenos", "language", "style"]
+        owner = serializers.ReadOnlyField(source="owner.username")
 
     def create(self, validated_data):
         """Create and return a new snippet instance, given the validated data."""
--- snippets/views.py
+++ snippets/views.py
@@ -1,15 +1,32 @@
-from rest_framework import generics
+from django.contrib.auth.models import User
+
+from rest_framework import generics, permissions
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """Retrieve, update or delete a snippet instance."""
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
